3e2e3b3b508dbd9eba0a0925d56136ad59292f09
RicardoMVA <ricardo-valenca@hotmail.com>
2020-08-17 18:36:17 -0300

    cria classe 'user'

---
+++ app/src/main/java/com/br/recode/bancode/model/User.java
@@ -0,0 +1,60 @@
+package com.br.recode.bancode.model;
+
+public class User {
+    private int cpf;
+    private String name;
+    private String avatar;
+    private int telefone;
+    private int pws;
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getPws() {
+        return pws;
+    }
+
+    public void setPws(int pws) {
+        this.pws = pws;
+    }
+
+    @Override
+    public String toString() {
+        return "User {" +
+                "cpf=" + cpf +
+                ", name='" + name + '\'' +
+                ", avatar='" + avatar + '\'' +
+                ", telefone=" + telefone +
+                ", pws=" + pws +
+                '}';
+    }
+}
