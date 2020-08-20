0bc3fade60a786c15733ae2a4e42521325e665f9
RicardoMVA <ricardo-valenca@hotmail.com>
2020-08-20 20:43:43 -0300

    resolve problemas de dependência

--- app/src/main/java/com/br/recode/bancode/MovimentacaoActivity.java
+++ app/src/main/java/com/br/recode/bancode/MovimentacaoActivity.java
@@ -19,6 +19,6 @@ public class MovimentacaoActivity extends AppCompatActivity {
 
         RecyclerView.LayoutManager linearLayoutManager = new LinearLayoutManager(this);
         recyclerMovimentacao.setLayoutManager(linearLayoutManager);
-        recyclerMovimentacao.setAdapter(new ListaMovimentacaoAdapter());
+//        recyclerMovimentacao.setAdapter(new ListaMovimentacaoAdapter());
     }
 }
