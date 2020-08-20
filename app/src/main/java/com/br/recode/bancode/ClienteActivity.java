561e64ab789af57c34e3c825913608045b6444e7
RicardoMVA <ricardo-valenca@hotmail.com>
2020-08-19 21:55:53 -0300

    faz botão de editar cliente na cliente activity mandar para a activity de editar

--- app/src/main/java/com/br/recode/bancode/ClienteActivity.java
+++ app/src/main/java/com/br/recode/bancode/ClienteActivity.java
@@ -32,6 +32,6 @@ public class ClienteActivity extends AppCompatActivity {
                 intent.putExtra("user", usuario);
                 startActivity(intent);
             }
-        }
-    });
+        });
+    };
 }
