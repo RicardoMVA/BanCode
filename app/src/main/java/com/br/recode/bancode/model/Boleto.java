c8ea17b592d2eb58cf3968a3530ec1c6d449e29f
Jonas Lucena <jonaslucena@outlook.com>
2020-08-22 20:16:21 -0300

    criacao classe boleto

---
+++ app/src/main/java/com/br/recode/bancode/model/Boleto.java
@@ -0,0 +1,33 @@
+package com.br.recode.bancode.model;
+
+public class Boleto {
+
+    private String codigoBoleto;
+
+    public String getCodigoBoleto() {
+        return codigoBoleto;
+    }
+
+    public String gerarCodigoBoleto(Conta conta ,double valor) {
+        String numeroDaConta = String.format ("%07d", conta.getCode());
+        String valorDoBoleto = String.format ("%09d", valor);
+
+        return codigoBoleto = numeroDaConta + valorDoBoleto;
+    }
+
+    public int getContaBoleto() {
+        codigoBoleto = getCodigoBoleto();
+
+        int contaBoleto = Integer.valueOf(codigoBoleto.substring(0, 8));
+
+        return contaBoleto;
+    }
+
+    public double getValorBoleto() {
+        codigoBoleto = getCodigoBoleto();
+
+        int valorBoleto = Integer.valueOf(codigoBoleto.substring(8));
+
+        return valorBoleto;
+    }
+}
